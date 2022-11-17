3c350811a3050df485eb87c3d7f61eda7136b422
Anna Lander <anna.lander@selu.edu>
2022-11-17 20:31:54 +0000

    Final Project Package

---
+++ R/MutateFilter.R
@@ -0,0 +1,9 @@
+#'Brief sentence of what is it - Sort the data by the desired variables
+ 
+#' @param Define function variables
+#' 
+#' @return Define return statement(tell userthey'll know that the function is returning right type)
+#' 
+#' @export
+#' 
+#' 
