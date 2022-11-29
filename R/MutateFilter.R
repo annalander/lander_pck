7d404b7e612e381699a86e6aff73cd9f2165cb68
Anna Lander <anna.lander@selu.edu>
2022-11-29 18:44:47 +0000

    all changes

--- R/MutateFilter.R
+++
@@ -1,9 +0,0 @@
-#'Brief sentence of what is it - Sort the data by the desired variables
- 
-#' @param Define function variables
-#' 
-#' @return Define return statement(tell userthey'll know that the function is returning right type)
-#' 
-#' @export
-#' 
-#' 
