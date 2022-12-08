1cb1f94dc510f0a559878bf6fbec94da36bd9a09
Anna Lander <anna.lander@selu.edu>
2022-12-08 23:44:49 +0000

    Now should be good, README, Package tutorial, functions

--- R/LinModPlot.R
+++ R/LinModPlot.R
@@ -1,13 +1,13 @@
 #' This function shows the relationship between mass and snout-vent length of amphibians
 #'  
-#' @param critter
+#' @param AGP
 #' 
 #' @return point plot displaying relationship between two variables 
 #' 
 #' @examples point_plot(AGP)
 
 
-point_plot <- function(critter){
+point_plot <- function(AGP){
   AGP %>% 
     filter(critter == "Tiger Salamander") 
   AGPpoint <- ggplot(data = AGP, mapping = aes(x = svl, y = mass, color = stage)) + geom_point() + xlim(0,150) + ylim(0,150) + labs(title = "Relationship Between Mass and Snout-Vent Length in Amphibians", x = "Snout-Vent Length (mm)", y = "Mass (g)")
